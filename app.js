b2b1f946fd47268fd9d74dd36ce2ca42b89a13f7
Leonard Law <leon.wlaw@gmail.com>
2017-11-22 20:31:04 -0500

    Promisify tutorial code
    Remove callback hell to improve readability

--- app.js
+++ app.js
@@ -4,76 +4,71 @@ var assert = require('assert');
 var url = 'mongodb://localhost:27017/myproject';
 
 
-function insertDocuments(db, callback) {
+function insertDocuments(db) {
   // Grab the documents collection.
   const collection = db.collection('documents');
 
   const documents = [{a: 1}, {a: 2}, {a: 3}];
-  collection.insertMany(documents, function(err, result) {
-    assert.equal(null, err);
+
+  return collection.insertMany(documents).then((result) => {
     assert.equal(3, result.result.n);
     assert.equal(3, result.ops.length);
     console.log("Inserted 3 documents into the collection!");
-    callback(result);
   });
 }
 
-function findDocuments(db, callback) {
+function findDocuments(db) {
   // Grab the documents collection.
   const collection = db.collection('documents');
 
   // Only retrieve documents that match our query.
   // Queries are plain JS objects.
   const query = {a: 2};
-  collection.find(query).toArray(function(err, docs) {
-    assert.equal(null, err);
+
+  return collection.find(query).toArray().then((docs) => {
     console.log("Found the following records:");
     console.log(docs);
-    callback(docs);
-  })
+  });
 }
 
 
-function updateDocument(db, callback) {
+function updateDocument(db) {
   const collection = db.collection('documents');
 
   // Update the first document matching the filter a === 2, setting b = 1.
   const filter = {a: 2};
   const update = {$set: {b: 1}};
-  collection.updateOne(filter, update, function(err, result) {
-    assert.equal(null, err);
+
+  return collection.updateOne(filter, update).then((result) => {
     assert.equal(1, result.result.n);
     console.log("Successfully updated a document by setting b = 1.");
-    callback(result);
   });
 }
 
 
-function removeDocument(db, callback) {
+function removeDocument(db) {
   const collection = db.collection('documents');
 
   // Delete the first document where a is 3
   const filter = {a: 3};
-  collection.deleteOne(filter, function(err, result) {
-    assert.equal(null, err);
+
+  return collection.deleteOne(filter).then((result) => {
     assert.equal(1, result.result.n);
     console.log('Removed a document matching a = 3!');
-    callback(result);
   });
 }
 
 
-function indexCollection(db, callback) {
+function indexCollection(db) {
   const collection = db.collection('documents');
 
   // It appears that we only need to specify the keys, the value we actually
   // store in the object is not important.
   const fields = {a: 1};
   const options = null;
-  collection.createIndex(fields, options, function(err, results) {
-    assert.equal(null, err);
+
+  return collection.createIndex(fields, options).then((results) => {
     console.log(`Added an index to field 'a' as "${results}"!`);
-    callback(results);
   });
 }
 
@@ -83,15 +78,10 @@ MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server!");
 
-  indexCollection(db, function() {
-    insertDocuments(db, function() {
-      updateDocument(db, function() {
-        findDocuments(db, function() {
-          removeDocument(db, function() {
-            db.close();
-          });
-        });
-      });
-    });
-  });
+  indexCollection(db)
+    .then(() => insertDocuments(db))
+    .then(() => updateDocument(db))
+    .then(() => findDocuments(db))
+    .then(() => removeDocument(db))
+    .then(() => db.close());
 });
